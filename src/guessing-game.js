7d30d155ff135c4b31d1a169051727a4646ef222
saloev <saloev.saadi@yandex.ru>
2019-10-05 20:30:41 +0300

    feat: add setRange, guess, lower methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
 
     }
 
     guess() {
+        this.middle = Math.round((this.min + this.max) / 2);
 
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
